consuldemocracy/consuldemocracy

Verify poll ballots

db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180621182723) do
+ActiveRecord::Schema.define(version: 20180704095538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -136,9 +136,11 @@ ActiveRecord::Schema.define(version: 20180621182723) do
   create_table "budget_ballots", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "budget_id"
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
     t.integer  "ballot_lines_count", default: 0
+    t.boolean  "physical",           default: false
+    t.integer  "poll_ballot_id"
   end
 
   create_table "budget_groups", force: :cascade do |t|
@@ -785,6 +787,14 @@ ActiveRecord::Schema.define(version: 20180621182723) do
 
   add_index "poll_ballot_sheets", ["officer_assignment_id"], name: "index_poll_ballot_sheets_on_officer_assignment_id", using: :btree
   add_index "poll_ballot_sheets", ["poll_id"], name: "index_poll_ballot_sheets_on_poll_id", using: :btree
+
+  create_table "poll_ballots", force: :cascade do |t|
+    t.integer  "ballot_sheet_id"
+    t.text     "data"
+    t.integer  "external_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
   create_table "poll_booth_assignments", force: :cascade do |t|
     t.integer  "booth_id"

viren140290/consul

db/schema.rb
@@ -1279,6 +1279,12 @@ ActiveRecord::Schema.define(version: 20180718115545) do
   add_index "votes", ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope", using: :btree
   add_index "votes", ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope", using: :btree
 
+  create_table "web_sections", force: :cascade do |t|
+    t.text     "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "widget_cards", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
@@ -1295,12 +1301,6 @@ ActiveRecord::Schema.define(version: 20180718115545) do
     t.integer  "limit",      default: 3
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
-  end
-
-  create_table "web_sections", force: :cascade do |t|
-    t.text     "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "administrators", "users"